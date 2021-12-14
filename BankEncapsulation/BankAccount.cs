07d48cb9aafcf92bfaae0c64d0b53614a3f7f9e0
Tim Honchel <tim.honchel@gmail.com>
2021-12-14 14:53:56 -0500

    first commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userBankAccount = new BankAccount();
+            
+            Menu(userBankAccount);
+        }
+
+        public static void Menu(BankAccount userBankAccount)
+        {
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("Enter a number to select an option from the menu:");
+            Console.WriteLine();
+            Console.WriteLine("1. Make a deposit.");
+            Console.WriteLine("2. Check your balance.");
+            Console.WriteLine("3. Exit the program.");
+            Console.WriteLine();
+            
+            string option = Console.ReadLine();
+            Console.WriteLine();
+
+            if (option == "1")
+            {
+                Console.WriteLine("How much would you like to deposit?");
+                Console.WriteLine();
+                double deposit = Convert.ToDouble(Console.ReadLine());
+                userBankAccount.Deposit(deposit);
+                Console.WriteLine();
+                Console.WriteLine($"${deposit} has been deposited to your account.");
+                Console.WriteLine();
+                Menu(userBankAccount);
+            }
+            else if (option == "2")
+            {
+                Console.WriteLine($"Your account balance is ${userBankAccount.GetBalance()}");
+                Console.WriteLine();
+                Menu(userBankAccount);
+            }
+            else if (option == "3")
+            {
+                Console.WriteLine("Have a great day!");
+                // ends program
+            }
+            else
+            {
+                Menu(userBankAccount); //resets menu
+            }
+
+        }
+    }
+}
